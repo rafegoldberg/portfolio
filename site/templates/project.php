a0303d3e60a8df3787dfe5fe3de887b955b431ef
Rafe <rafegoldberg@gmail.com>
2015-04-30 16:12:52 -0400

    minor changes

--- site/templates/project.php
+++ site/templates/project.php
@@ -10,13 +10,13 @@
 
 		<nav class="grid-12 parent nextprev cf block-center-solo ta-center soft--links" role="navigation" style="background-color:<?=$page->fg_color()?>;color:<?=$page->bg_color()?>;">
 			<? if($next = $page->nextVisible()): ?>
-				<a class="next grid-6 card_huge" href="<?= $next->url() ?>">
+				<a class="next grid-6 card_big" href="<?= $next->url() ?>">
 					<span class="iconic d-ib va-m gutter-right" data-glyph="caret-left"></span>
 					<span class="va-m d-ib">Prior Project</span>
 				</a>
 			<? endif ?>
 			<? if($prev = $page->prevVisible()): ?>
-				<a class="prev grid-6 card_huge" href="<?= $prev->url() ?>">
+				<a class="prev grid-6 card_big" href="<?= $prev->url() ?>">
 					<span class="va-m d-ib">Next Project</span>
 					<span class="iconic d-ib va-m gutter-left" data-glyph="caret-right"></span>
 				</a>
