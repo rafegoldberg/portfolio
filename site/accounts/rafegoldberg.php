b0f660cf218fa1260ff8af291a344099be2554a2
Rafe Goldberg <rafegoldberg@gmail.com>
2015-05-13 08:12:35 -0400

    Squashed commit of the following:
    commit 822a2a2b3efebba76d89ac46217d875819910fb3
    Author: Rafe Goldberg <rafegoldberg@gmail.com>
    Date:   Wed May 13 08:11:46 2015 -0400
    
        Add clients subpage
    
        some css updates as well

--- site/accounts/rafegoldberg.php
+++ site/accounts/rafegoldberg.php
@@ -6,7 +6,6 @@ password: >
   $2a$10$2uQFoPKLAh5MLEtSK7yqBe8NcOAPKvxmkixrSdtb5vCOY8XgttQ.C
 language: en
 role: admin
-token: 5b33c9fe88a133f481dc3b2e0f72797f17580bde
 history:
   - null
   - projects/ramsay-for-la
